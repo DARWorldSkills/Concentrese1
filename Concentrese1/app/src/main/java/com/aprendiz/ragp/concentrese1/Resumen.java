ee1303c065427b84ec1a85f58567c504c0d64f4b
DavidPyn27 <popayan159@gmail.com>
2018-08-14 16:37:20 -0500

    xxxxxxx

---
+++ Concentrese1/app/src/main/java/com/aprendiz/ragp/concentrese1/Resumen.java
@@ -0,0 +1,36 @@
+package com.aprendiz.ragp.concentrese1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Resumen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumen);
+    }
+
+    public void Compartir(View view) {
+
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.setPackage("com.twitter.android");
+        intent.putExtra(Intent.EXTRA_TEXT, "");
+
+        try {
+            startActivity(intent);
+
+        }catch (Exception e){
+            Toast.makeText(this, "No cuentas con está app", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void Face(View view) {
+    }
+}
